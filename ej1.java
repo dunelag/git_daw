855063ef16c467065b78bdfc36e07b7581a9ef92
dani <dgonzalezj001@gmail.com>
2026-02-04 21:02:52 +0100

    version 2 de mi archivo

--- ej1.java
+++ ej1.java
@@ -4,16 +4,22 @@ public class ej1 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int[] numeros = new int[5];
+        int suma = 0;
 
         for (int i = 0; i < 5; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = teclado.nextInt();
+            suma += numeros[i];
         }
 
         System.out.println("\nLos números que ingresaste son:");
         for (int i = 0; i < numeros.length; i++) {
             System.out.println("Número " + (i + 1) + ": " + numeros[i]);
         }
+
+        double media = (double) suma / numeros.length;
+        System.out.println("Suma: " + suma);
+        System.out.println("Media: " + media);
 
         teclado.close();
     }
