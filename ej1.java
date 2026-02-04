99b744738fa473f5f8c87817aea9fe7342eaf786
dani <dgonzalezj001@gmail.com>
2026-02-04 21:05:58 +0100

    version 3 de mi archivo

--- ej1.java
+++ ej1.java
@@ -5,11 +5,25 @@ public class ej1 {
         Scanner teclado = new Scanner(System.in);
         int[] numeros = new int[5];
         int suma = 0;
+        int mayor = Integer.MIN_VALUE;
+        int menor = Integer.MAX_VALUE;
+        int positivos = 0;
+        int negativos = 0;
 
         for (int i = 0; i < 5; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = teclado.nextInt();
             suma += numeros[i];
+
+            if (numeros[i] > mayor)
+                mayor = numeros[i];
+            if (numeros[i] < menor)
+                menor = numeros[i];
+
+            if (numeros[i] > 0)
+                positivos++;
+            else if (numeros[i] < 0)
+                negativos++;
         }
 
         System.out.println("\nLos números que ingresaste son:");
@@ -20,6 +34,10 @@ public class ej1 {
         double media = (double) suma / numeros.length;
         System.out.println("Suma: " + suma);
         System.out.println("Media: " + media);
+        System.out.println("Mayor: " + mayor);
+        System.out.println("Menor: " + menor);
+        System.out.println("Cantidad de números positivos: " + positivos);
+        System.out.println("Cantidad de números negativos: " + negativos);
 
         teclado.close();
     }
